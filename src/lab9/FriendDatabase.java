6b9af0b5816c41350a96ec10d7209aa7c667ebfc
bassam-14 <mohamedbassam1442004.wednesday@gmail.com>
2024-12-05 02:36:51 +0200

    Create FriendDatabase.java

---
+++ src/lab9/FriendDatabase.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab9;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import java.util.*;
+
+/**
+ *
+ * @author bassam
+ */
+public class FriendDatabase extends FileHandling<FriendRequest>{
+    private static FriendDatabase instance;
+      private FriendDatabase(){
+       super("friendrequests.json",new TypeReference<List<FriendRequest>>(){});
+    }
+    public static FriendDatabase getInstance(){
+        if(instance==null)
+            instance=new FriendDatabase();
+        return instance;
+    }
+      @Override
+    public FriendRequest getRecord(String friendShipId){
+    for(FriendRequest f:records){
+        if(f.getFriendShipId().equals(friendShipId))
+            return f;
+    }
+    return null;
+}
+    public List<FriendRequest> getSentRequests(String userId){
+        List<FriendRequest> sentRequests=new ArrayList<>();
+        for(FriendRequest f:records){
+            if(f.getSenderID().equals(userId)&&f.getStatus().equals("Pending"))
+                sentRequests.add(f);
+        }
+        return sentRequests;
+    }
+        public List<FriendRequest> getRecievedRequests(String userId){
+        List<FriendRequest> recievedRequests=new ArrayList<>();
+        for(FriendRequest f:records){
+            if(f.getRecieverID().equals(userId)&&f.getStatus().equals("Pending"))
+                recievedRequests.add(f);
+        }
+        return recievedRequests;
+    }
+        public List<String> getFriends(String userId){
+        List<String> friends=new ArrayList<>();
+        for(FriendRequest f:records){
+            if(f.getRecieverID().equals(userId)&& f.getStatus().equals("Accepted"))
+                friends.add(f.getSenderID());
+             if(f.getSenderID().equals(userId)&& f.getStatus().equals("Accepted"))
+                friends.add(f.getRecieverID());
+        }
+        return friends;
+    }
+        
+        
+    
+}
